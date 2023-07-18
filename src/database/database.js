9214f85fd617754f33a61069e0a58384efb47037
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-18 17:03:09 -0300

    Database configuration

---
+++ src/database/database.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+
+dotenv.config();
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+try {
+    await mongoClient.connect();
+    console.log('Connected to MongoDB');
+} catch (error) {
+    console.log(error.message);
+}
+
+export const db = mongoClient.db();

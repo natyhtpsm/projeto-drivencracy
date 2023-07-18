b96563e9e2cdca32b3c18f7e95d9cadaaf34831a
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-18 17:10:54 -0300

    Database connection

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import router from './routes';
 
 const app = express();
-
 app.use(cors());
 app.use(express.json()); 
-app.use(router);
-
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server listen on port ${port}`));
+app.listen(port, () => console.log(`Server listening on port ${port}`));

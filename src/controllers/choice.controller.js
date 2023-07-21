76581cae6d9089afb18d2287539f5057c933881e
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-21 12:42:48 -0300

    Fix: vote choice
    fixed bug where the choice does not exist

--- src/controllers/choice.controller.js
+++ src/controllers/choice.controller.js
@@ -3,7 +3,6 @@ import dayjs from 'dayjs';
 import { ObjectId } from 'mongodb';
 
 export async function choiceController(req, res){
-    // - [ ]  Deve receber pelo body da request, um parâmetro title, contendo o nome da opção a ser cadastrada e pollId.
     const {title, pollId} = req.body;
     const choice = {title, pollId};
     try{
@@ -11,16 +10,13 @@ export async function choiceController(req, res){
         const choiceExists =  await db.collection(collections.choices).findOne({title : title, pollId: pollId});
         
         if(!pollExists){
-            //     - [ ]  Uma opção de voto não pode ser inserida sem uma enquete existente, retornar status 404.
             return res.status(404).send("Poll does not exist");
         }
         if(choiceExists){
-            //     - [ ]  **Title** não pode ser repetido, retornar status 409.
             return res.status(409).send("Choice already exists");
         }
         const pollExpired = pollExists.expiredAt;
         if(dayjs(pollExpired).isBefore(dayjs())){
-            //     - [ ]  Se a enquete já estiver expirado deve retornar erro com status 403.
             return res.status(403).send("Poll expired");
         }
         const choiceCreated = await db.collection(collections.choices).insertOne(choice);
@@ -63,13 +59,14 @@ export async function voteController(req, res){
     const vote = {createdAt: dayjs().format('YYYY-MM-DD HH:mm'), choiceId: id};
     const currentDate = dayjs();
     try{
-        const choiceExists = await db.collection(collections.choices).findOne({ _id: id });
+        const choiceExists = await db.collection(collections.choices).findOne({ _id: new ObjectId(id) });
         //     - [ ]  Verificar se é uma opção existente, se não existir retornar 404.
+        console.log("CHOICE", choiceExists);
         if (!choiceExists) {
           return res.status(404).send("This choice does not exist");
         }  
 
-        const poll = await db.collection(collections.polls).findOne({ _id: choiceExists.pollId });
+        const poll = await db.collection(collections.polls).findOne({ _id: new ObjectId(choiceExists.pollId)});
         if (!poll) {
           return res.status(404).send("This poll does not exist");
         }

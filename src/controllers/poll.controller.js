eb4efb2c22f862d1bd497d0734a7adc88d32fc2f
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-07-21 13:45:22 -0300

    Feat: results

--- src/controllers/poll.controller.js
+++ src/controllers/poll.controller.js
@@ -1,5 +1,6 @@
 import { db, collections } from '../database/database.js';
 import dayjs from 'dayjs';
+import { ObjectId } from 'mongodb';
 
 export async function pollController(req, res){
     // - [ ]  Deve receber pelo body da request, um parâmetro title, contendo o nome da enquete a ser cadastrada e expireAt, contendo a data e hora de expiração da enquete:
@@ -44,3 +45,63 @@ export async function getpollController(req, res){
 
 }
 
+export async function getResultController(req, res) {
+    const id = req.params.id;
+  
+    try {
+      const poll = await db.collection(collections.polls).findOne({ _id: new ObjectId(id) });
+      if (!poll) {
+        return res.status(404).send("This poll does not exist");
+      }
+  
+      const choices = await db.collection(collections.choices).find({ pollId: id }).toArray();
+  
+      if (choices.length === 0) {
+        return res.status(404).send("There are no choices for this poll");
+      }
+
+      const choiceIds = choices.map((choice) => choice._id);
+      const voteCounts = await db.collection(collections.votes).aggregate([
+        { $match: { choiceId: { $in: choiceIds } } }, 
+        { $sortByCount: "$choiceId" }, 
+      ]).toArray();
+  
+      if (voteCounts.length === 0) {
+        return res.status(404).send("No votes have been cast for this poll");
+      }
+
+      const mostVotedChoiceId = voteCounts[0]._id;
+      const mostVotedChoice = choices.find((choice) => choice._id.equals(mostVotedChoiceId));
+  
+      const result = {
+        _id: poll._id,
+        title: poll.title,
+        expireAt: poll.expireAt,
+        result: {
+          title: mostVotedChoice.title,
+          votes: voteCounts[0].count,
+        },
+      };
+  
+      return res.json(result);
+    } catch (e) {
+      console.log(e.message);
+    }
+}
+
+// - **GET** `/poll/:id/result`
+//     - [ ]  Retorna o resultado de uma enquete, ou seja, a opção de voto **mais votada** na enquete até o momento, seguindo o formato sugerido:
+    
+//     ```jsx
+//     {
+//     	_id: "54759eb3c090d83494e2d222",
+//     	title: "Qual a sua linguagem de programação favorita?"
+//     	expireAt: "2022-02-14 01:00",
+//     	result : {
+//     		title: "Javascript",
+//     		votes: 487
+//     	}
+//     }
+//     ```
+    
+//     - [ ]  Validação: caso a enquete não exista deve retornar status 404.
--- src/routes/poll.routers.js
+++ src/routes/poll.routers.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { pollController, getpollController } from '../controllers/poll.controller.js';
+import { pollController, getpollController, getResultController } from '../controllers/poll.controller.js';
 import pollMiddleware  from '../middlewares/poll.middleware.js';
 
 export const pollRouter = Router();
 
 pollRouter.post('/poll', pollMiddleware, pollController);
 pollRouter.get('/poll', getpollController);
+pollRouter.get('/poll/:id/result', getResultController);
